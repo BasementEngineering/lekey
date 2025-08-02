e646a22ad3d0e8d8350132913f0fa3febca6589e
TransitSenseDev <j.kettler@transitsense.de>
2025-08-02 14:01:01 +0200

    Integrated new display ui into main firmware

--- LEKeys_Firmware/LEKeys_Firmware.ino
+++ LEKeys_Firmware/LEKeys_Firmware.ino
@@ -2,37 +2,38 @@
 #include <MD_MIDIFile.h>
 
 #include "led_output.h"
+#include "display_output.h"
+
 #include "pins.h"
-
-int playback_speed_bpm = 60; // Playback speed in beats per minute
-/*
-char* tune_list[] = {
-    "6LEDS.MID", // Simple and short file
-    NULL // End of list
-};
-
-*/
-
-int choosen_tune = 0; // Index of the currently chosen tune
-
-// OLED Dsiplay
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET    -1 // Reset pin not used
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#include "user_interface.h"
 
 #include "encoder_input.h"
 #include "button_input.h"
 
+int playback_speed_bpm = 60; // Playback speed in beats per minute
+int choosen_tune = 0; // Index of the currently chosen tune
+
+int bpm = 120; // Beats per minute
+int beatsPerBar = 4; // Beats per measure
+int currentBeat = 0; // Current beat in the measure
+int currentBar = 0; // Current bar in the song
+int maxBars = 120; // Total number of bars in the song
+int loopLength = 3; // Length of the loop in bars
+int loopStart = 4;
+
+
+enum class DeviceState {
+    INITIALIZING1,
+    INITIALIZING2,
+    PAUSED,
+    PLAYING
+};
+DeviceState currentDeviceState = DeviceState::INITIALIZING1;
+unsigned long lastStateChange = 0;
+
 Encoder_Input encoder(ENCODER_CLK_PIN, ENCODER_DT_PIN);
 Button_Input buttonInput(ENCODER_SW_PIN, 20); // Debounce delay 50ms
-UserInterface ui(&display, &playback_speed_bpm, &encoder, &buttonInput);
 LedStrip strip(LED_PIN); // Create a LedStrip object with 61 LEDs (5 octaves + 1 for the highest note)
-
+Display_Output display;
 
 SdFat	SD;
 MD_MIDIFile SMF;
@@ -53,9 +54,158 @@ MD_MIDIFile SMF2; // Second MIDI file object that is going to point to the same 
 // or an error will occur opening the file and the next in the 
 // list will be opened (skips errors).
 const char *loopfile = "wonderwall.mid";  // simple and short file
-
 int max_files = 10;
 char **file_list = NULL; // Pointer to an array of file names
+
+// Function Prototypes
+void runStateMachine();
+void switchState(DeviceState newState);
+void fillFileListFromSD();
+void setupPlayer();
+void midiCallback(midi_event *pev);
+void midiCallback2(midi_event *pev);
+void main_button_isr();
+void setupLeds();
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Starting Keyboard");
+    delay(1000);
+    
+    display.begin(); // Initialize the user interface
+    switchState(DeviceState::INITIALIZING1); // Set initial state to INITIALIZING1
+    runStateMachine(); // Run the state machine to update the display
+
+    setupLeds(); // Setup the LED strip
+    
+    #define SPI_SPEED SD_SCK_MHZ(4) //Does not work without that
+    if (!SD.begin(SD_CS_PIN, SPI_SPEED))
+    {
+        Serial.println("\nSD init fail!");
+        while (true) ;
+    }
+
+    fillFileListFromSD(); // Fill the file list from the SD card
+    setupPlayer(); // Setup the MIDI player
+
+    encoder.begin();
+    buttonInput.begin(main_button_isr);
+
+    display.begin();
+    display.update(); // Initial display update
+
+    Serial.println("Ready");
+}
+
+unsigned long tick_length = 100;
+unsigned long last_tick = 0;
+
+unsigned long current_tick = 0;
+int last_bpm = 0; // Last BPM value to avoid unnecessary updates
+bool rewinded = false; // Flag to indicate if the file has been rewound
+
+void loop() {
+     handleBpmChange(); // Handle BPM changes
+
+    if (!SMF2.isEOF())
+    {
+      SMF2.getNextEvent();
+    }
+
+    if(!rewinded && millis() > 500){
+      SMF2.restart(); // Rewind the second MIDI file
+      rewinded = true; // Set the flag to true to avoid rewinding again
+      Serial.println("MIDI file rewound");
+    }
+    runStateMachine(); // Run the state machine to update the display and handle events
+// Remove encoder.process() from loop and use a timer interrupt instead
+}
+
+////// STATE MACHINE FUNCTIONS //////
+void runStateMachine() {
+    switch (currentDeviceState) {
+    case DeviceState::INITIALIZING1:
+        if (millis() - lastStateChange > 1000) { // Update every second
+            switchState(DeviceState::INITIALIZING2); // Switch to INITIALIZING2 after 1 second
+        }
+        break;
+    case DeviceState::INITIALIZING2:
+        if (millis() - lastStateChange > 1000) { // Update every second
+            switchState(DeviceState::PLAYING); // Switch to PAUSED after 1 second
+        }
+        break;
+    case DeviceState::PAUSED:
+        
+        break;
+    case DeviceState::PLAYING:
+        if (millis() - last_tick > 200) { // Update every 200 milliseconds
+            SMF.getNextEvent(); // Process the next MIDI event
+            display.showPlaying(bpm, (char *)loopfile, true, currentBar, maxBars, currentBeat, loopStart, loopLength);
+            last_tick = millis();
+        }
+        break;
+    }
+}
+
+void switchState(DeviceState newState) {
+  Serial.print("Switching state from ");
+  Serial.println(static_cast<int>(currentDeviceState));
+  Serial.print(" to ");
+  Serial.println(static_cast<int>(newState));
+
+  currentDeviceState = newState;
+  lastStateChange = millis(); // Reset the timer when switching states
+
+  switch (newState) {
+    case DeviceState::INITIALIZING1:
+      display.showInitializing(1); // Show initializing screen 1
+      break;
+    case DeviceState::INITIALIZING2:
+      display.showInitializing(0); // Show initializing screen 2
+      break;
+    case DeviceState::PAUSED:
+      break;
+    case DeviceState::PLAYING:
+      display.showPlaying(bpm, (char *)loopfile, true, currentBar, maxBars, currentBeat, loopStart, loopLength); // Show playing screen
+      break;
+  }
+}
+
+////// SETUP FUNCTIONS //////
+void setupPlayer(){
+ // Initialize MIDIFile
+    SMF.begin(&SD);
+    SMF2.begin(&SD);
+    SMF.setMidiHandler(midiCallback);
+    SMF2.setMidiHandler(midiCallback2);
+    SMF.looping(true);
+    SMF2.looping(true);
+
+    int err;
+    err = SMF.load(loopfile);
+  if (err != MD_MIDIFile::E_OK)
+  {
+    Serial.println("SMF load Error ");
+    
+    while (true);
+  }
+
+    err = SMF2.load(loopfile);
+  if (err != MD_MIDIFile::E_OK)
+  {
+    Serial.println("SMF2 load Error "); 
+    while (true);
+  }
+    Serial.print("SMF loaded: ");
+    Serial.println(loopfile);
+}
+
+void setupLeds() {
+    strip.begin(); // Initialize the LED strip
+    strip.runTestSequence(); // Run the test sequence to check if the LEDs are working
+    strip.setMainColor(strip.Color(0, 0, 255)); // Set main color to blue
+    strip.setForeshadowColor(strip.Color(127, 0, 0)); // Set foreshadow color to dim red
+}
 
 void fillFileListFromSD() {
   Serial.println("Filling file list from SD...");
@@ -104,6 +254,19 @@ void fillFileListFromSD() {
   Serial.println(count);
 }
 
+/////// INPUT HANDLING FUNCTIONS //////
+void handleBpmChange() {
+    if(last_bpm != playback_speed_bpm) {
+        last_bpm = playback_speed_bpm; // Update last BPM
+        tick_length = 60000 / playback_speed_bpm; // Calculate new tick length based on BPM
+        Serial.print("Tick length updated to: ");
+        Serial.println(tick_length);
+        SMF.setTempo(playback_speed_bpm); // Update the tempo in the MIDI file
+        SMF2.setTempo(playback_speed_bpm); // Update the tempo in the second MIDI file
+    }
+}
+
+////// MIDI CALLBACK FUNCTIONS //////
 void midiCallback2(midi_event *pev) {
     Serial.print("MIDI Event 2: ");
     Serial.print("Track: ");
@@ -157,137 +320,4 @@ void midiCallback(midi_event *pev) {
 
 void main_button_isr(){
   buttonInput.handleInterrupt();
-}
-
-void setupPlayer(){
- // Initialize MIDIFile
-    SMF.begin(&SD);
-    SMF2.begin(&SD);
-    SMF.setMidiHandler(midiCallback);
-    SMF2.setMidiHandler(midiCallback2);
-    SMF.looping(true);
-    SMF2.looping(true);
-
-    int err;
-    err = SMF.load(loopfile);
-  if (err != MD_MIDIFile::E_OK)
-  {
-    Serial.println("SMF load Error ");
-    
-    while (true);
-  }
-
-    err = SMF2.load(loopfile);
-  if (err != MD_MIDIFile::E_OK)
-  {
-    Serial.println("SMF2 load Error "); 
-    while (true);
-  }
-    Serial.print("SMF loaded: ");
-    Serial.println(loopfile);
-}
-
-void setupLeds() {
-    strip.begin(); // Initialize the LED strip
-    strip.runTestSequence(); // Run the test sequence to check if the LEDs are working
-    strip.setMainColor(strip.Color(0, 0, 255)); // Set main color to blue
-    strip.setForeshadowColor(strip.Color(127, 0, 0)); // Set foreshadow color to dim red
-}
-
-void setup() {
-    Serial.begin(9600);
-    Serial.println("Starting Keyboard");
-    delay(1000);
-    setupLeds(); // Setup the LED strip
-    Wire.begin();
-    ui.begin(); // Initialize the user interface
-    
-    #define SPI_SPEED SD_SCK_MHZ(4) //Does not work without that
-    if (!SD.begin(SD_CS_PIN, SPI_SPEED))
-    {
-        Serial.println("\nSD init fail!");
-        while (true) ;
-    }
-
-    fillFileListFromSD(); // Fill the file list from the SD card
-    setupPlayer(); // Setup the MIDI player
-
-    encoder.begin();
-    buttonInput.begin(main_button_isr);
-
-    Serial.println("Ready");
-}
-
-unsigned long tick_length = 100;
-unsigned long last_tick = 0;
-
-unsigned long current_tick = 0;
-int last_bpm = 0; // Last BPM value to avoid unnecessary updates
-bool rewinded = false; // Flag to indicate if the file has been rewound
-
-void runStateMachine();
-
-void loop() {
-    if (millis() - last_tick >= tick_length) {
-        last_tick = millis();
-        current_tick++;
-    }
-
-      // play the file
-    if(last_bpm != playback_speed_bpm) {
-        last_bpm = playback_speed_bpm; // Update last BPM
-        tick_length = 60000 / playback_speed_bpm; // Calculate new tick length based on BPM
-        Serial.print("Tick length updated to: ");
-        Serial.println(tick_length);
-        SMF.setTempo(playback_speed_bpm); // Update the tempo in the MIDI file
-        SMF2.setTempo(playback_speed_bpm); // Update the tempo in the second MIDI file
-    }
-  /*if (!SMF.isEOF())
-  {
-    SMF.getNextEvent();
-  }*/
-  if (!SMF2.isEOF())
-  {
-    SMF2.getNextEvent();
-  }
-
-  if(!rewinded && millis() > 500){
-    SMF2.restart(); // Rewind the second MIDI file
-    rewinded = true; // Set the flag to true to avoid rewinding again
-    Serial.println("MIDI file rewound");
-  }
-
- ui.update(); // Update the user interface
-// Remove encoder.process() from loop and use a timer interrupt instead
-}
-
-
-enum class DeviceState {
-    INITIALIZING,
-    PAUSED,
-    PLAYING
-};
-
-DeviceState currentDeviceState = DeviceState::INITIALIZING;
-
-void runStateMachine() {
-    switch (currentDeviceState) {
-    case DeviceState::INITIALIZING:
-        if (millis() - last_tick > 1000) { // Update every second
-            ui.showInitializing();
-            last_tick = millis();
-            currentDeviceState = DeviceState::PAUSED; // Move to paused state after initializing
-        }
-        break;
-    case DeviceState::PAUSED:
-        
-        break;
-    case DeviceState::PLAYING:
-        if (millis() - last_tick > 200) { // Update every 200 milliseconds
-            SMF.getNextEvent(); // Process the next MIDI event
-            ui.showPlaying(playback_speed_bpm, SMF.getCurrentBar(), SMF.getMaxBars(), SMF.getCurrentBeat(), SMF.getLoopStart(), SMF.getLoopLength());
-            last_tick = millis();
-        }
-        break;
-    }
 }

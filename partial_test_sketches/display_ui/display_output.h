289c97eb7ce559b8594b282d0a7fe8ab3058577c
TransitSenseDev <j.kettler@transitsense.de>
2025-08-02 02:31:39 +0200

    Added display functions to show bars, beats and song title, still struggling with scrolling text

--- partial_test_sketches/display_ui/display_output.h
+++ partial_test_sketches/display_ui/display_output.h
@@ -30,6 +30,123 @@ public:
         display.display();
     }
 
+    void showInitializing() {
+        display.clearDisplay();
+        display.setTextSize(2);
+        display.setTextColor(SSD1306_WHITE);
+        display.drawBitmap(0, 0, bitmap_allArray[0], SCREEN_WIDTH, SCREEN_HEIGHT, SSD1306_WHITE);
+        display.display();
+    }
+
+    int filenameOffset = 0; // Offset for the filename display
+    int maxCharacetsOnScreen = 14; // Maximum characters to display on the screen
+    unsigned long lastTextUpdate = 0;
+    void drawFilename(const char* filename) {
+        display.setTextSize(1);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(12, 2);
+
+        int filenameLen = strlen(filename);
+        if (filenameLen > maxCharacetsOnScreen) {
+            //extend filename buffer with leading spaces
+            int leadingSpaces = maxCharacetsOnScreen - filenameOffset;
+            if (leadingSpaces < 0) {
+                leadingSpaces = 0; // Ensure no negative spaces
+            }
+            Serial.print("Leading spaces: ");
+            Serial.println(leadingSpaces);
+            for(int i = 0; i < leadingSpaces; i++) {
+                display.print(" ");
+            }
+
+            // Scroll filename from left to right
+            
+            if (filenameOffset > (filenameLen + maxCharacetsOnScreen) ) {
+                filenameOffset = 0; // Reset to start
+            }
+            
+            int shownTitleLength = maxCharacetsOnScreen - leadingSpaces;
+            if(shownTitleLength> 0){
+                char displayFilename[shownTitleLength+1] = {0};
+                Serial.println(filenameOffset);
+                Serial.println(shownTitleLength);
+                
+                strncpy(displayFilename, filename + filenameOffset - leadingSpaces, shownTitleLength);
+                Serial.print("Display Filename: ");
+                Serial.println(displayFilename);
+                display.print(displayFilename);
+            }
+
+            if ((millis() - lastTextUpdate) > 400) {
+                lastTextUpdate = millis();
+                filenameOffset++;
+            }
+        } else {
+            display.print(filename); // Display full filename
+            filenameOffset = 0; // Reset offset if filename fits
+        }
+    }
+
+    void showPlaying(int bpm, char* currentFilename, bool isPlaying, int currentBar, int maxBars, int currentBeat, int loopStart, int loopLength) {
+        display.clearDisplay();
+        drawFilename(currentFilename);
+        display.setTextSize(1);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(105, 14);
+        display.print(bpm);
+        display.setCursor(105, 24);
+        display.print("BPM");
+        
+        drawMetronome(currentBeat);
+        drawPlayButton(isPlaying); // Draw play button
+        draw_bar_boxes(currentBar, maxBars, loopStart, loopLength);
+        display.display();
+    }
+
+    void drawPlayButton(bool isPlaying){
+      if(isPlaying) {
+          //Draw Trangle Play Button in the upper left corner
+            display.fillTriangle(0, 0, 6, 5, 0, 10, SSD1306_WHITE); // Draw filled triangle for play button
+      } else {
+          // Draw two rectangles for pause button
+            display.fillRect(0, 0, 4, 10, SSD1306_WHITE); // Left rectangle
+            display.fillRect(6, 0, 4, 10, SSD1306_WHITE); // Right rectangle
+      }
+    }
+
+    void drawMetronome(int currentBeat) {
+        if(currentBeat % 2 == 0) {
+            display.drawCircle(115, 6, 5, SSD1306_WHITE); // Draw a circle for the beat
+        } else {
+            display.fillCircle(115, 6, 5, SSD1306_WHITE); // Fill circle for the beat
+        }
+    }
+
+    void draw_bar_boxes(int currentBar, int maxBars, int loopStart, int loopLength=1) {
+        const int barsShown = 4;
+        int pixelSpacing = 6;
+        int boxWidth = (SCREEN_WIDTH / barsShown) - pixelSpacing;
+        int boxHeight = 8;
+
+        display.setTextSize(1);
+        for (int i = 0; i < barsShown; i++) {
+            display.setCursor(i * (boxWidth + pixelSpacing) + 10, SCREEN_HEIGHT - 20);
+            display.print(currentBar + i + 1);
+
+            int offset = pixelSpacing + i * boxWidth + i * pixelSpacing;
+
+            if (i < loopLength) {
+                display.fillRect(offset, SCREEN_HEIGHT - 11, boxWidth, boxHeight, SSD1306_WHITE);
+            } else {
+                display.drawRect(offset, SCREEN_HEIGHT - 11, boxWidth, boxHeight, SSD1306_WHITE);
+            }
+
+            if(i == 0){
+              display.drawLine(offset, SCREEN_HEIGHT - 1, offset + boxWidth - 1, SCREEN_HEIGHT - 1, SSD1306_WHITE); // Draw line for current bar
+            }
+        }
+    }
+
     void update() {
         unsigned long now = millis();
         if (now - lastSwitch > switchInterval) {
--- partial_test_sketches/display_ui/display_ui.ino
+++ partial_test_sketches/display_ui/display_ui.ino
@@ -10,6 +10,70 @@ void setup() {
     display.update(); // Initial display update
 }
 
+
+
+int bpm = 120; // Beats per minute
+int beatsPerBar = 4; // Beats per measure
+int currentBeat = 0; // Current beat in the measure
+int currentBar = 0; // Current bar in the song
+int maxBars = 120; // Total number of bars in the song
+int loopLength = 3; // Length of the loop in bars
+int loopStart = 4;
+
+void updataValues(){
+    currentBeat++;
+    if (currentBeat % beatsPerBar == 0) {
+        currentBar++;
+        if (currentBar >= maxBars) {
+            currentBar = 0; // Reset to the first bar
+            currentBeat = 0; // Reset to the first beat
+        }
+    }
+}
+
+enum class DisplayState {
+    INITIALIZING,
+    PAUSED,
+    PLAYING
+};
+
+unsigned long lastUpdateTime = 0;
+DisplayState currentState = DisplayState::INITIALIZING;
+char* currentFilename = "Wonderwall extra long dong.mid"; // Current filename being played
+
+void stateMachine() {
+    switch (currentState)  // Switch statement to handle different states
+    {
+    case DisplayState::INITIALIZING:
+        if (millis() - lastUpdateTime > 1000) { // Update every second
+            display.showInitializing();
+            lastUpdateTime = millis();
+            currentState = DisplayState::PAUSED; // Move to paused state after initializing
+        }
+        break;
+    case DisplayState::PAUSED:
+        if (millis() - lastUpdateTime > 1000) { // Update every second
+                display.showPlaying(bpm, currentFilename, false,currentBar, maxBars, currentBeat, loopStart, loopLength);
+                lastUpdateTime = millis();
+                if( millis() > 5000) { // After 5 seconds, switch to playing state
+                    currentState = DisplayState::PLAYING; // Switch to playing state after 5 seconds
+                }
+            }
+        break;
+    case DisplayState::PLAYING:
+        if (millis() - lastUpdateTime > 200) { // Update every second
+            updataValues();
+            display.showPlaying(bpm, currentFilename, true, currentBar, maxBars, currentBeat, loopStart, loopLength);
+            lastUpdateTime = millis();
+        }
+        break;
+    
+    default:
+        break;
+    }
+}
+
 void loop() {
-    display.update(); // Update the display in the loop
+    //display.update(); // Update the display in the loop
+    stateMachine();
 }
